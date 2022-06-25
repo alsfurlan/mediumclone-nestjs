e78e224abb76a7f7edd441b85c05ac5a5591e4bb
Anderson Luís Furlan <alsfurlan@gmail.com>
2022-06-24 23:59:08 -0300

    Generating jwt token

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserEntity } from '@app/user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -15,5 +18,22 @@ export class UserService {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: { ...user, token: this.generateJwt(user) },
+    };
+  }
+
+  generateJwt({ id, username, email }: UserEntity): string {
+    return sign(
+      {
+        id,
+        username,
+        email,
+      },
+      JWT_SECRET,
+    );
   }
 }
